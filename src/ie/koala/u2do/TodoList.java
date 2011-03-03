a38916c290a22d8b4998a6d741350a17ac738077
simon <simon@koala.ie>
2011-03-03 13:20:50 +0000

    Signed-off-by: simon <simon@koala.ie>

---
+++ src/ie/koala/u2do/TodoList.java
@@ -0,0 +1,13 @@
+package ie.koala.u2do;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TodoList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
